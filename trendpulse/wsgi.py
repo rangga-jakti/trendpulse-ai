97dd6eec05a2a8bb48f6ddf77dc207d9e66778eb
Rangga <ranggarvc@gmail.com>
2026-03-21 01:34:44 +0700

    Add Vercel config

--- trendpulse/wsgi.py
+++ trendpulse/wsgi.py
@@ -1,4 +1,12 @@
+"""
+WSGI config for trendpulse project.
+"""
 import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trendpulse.settings')
+
 application = get_wsgi_application()
+
+# Vercel needs this
+app = application
